d073b224c9f33b2a28f2998a25ccd3d2fad57648
cfmarcos <marcoscatalanfernandez2@gmail.com>
2026-06-29 11:14:40 +0200

    Create fincas.js
    js de las fincas

---
+++ js/fincas.js
@@ -0,0 +1,114 @@
+const fincas = [
+
+{
+    id:1,
+    titulo:"Finca en Tudela",
+    comunidad:"Navarra",
+    provincia:"Navarra",
+    municipio:"Tudela",
+
+    precio:120,
+
+    metros:500,
+
+    destacada:true,
+
+    imagenes:[
+        "imagenes/finca1/1.jpg",
+        "imagenes/finca1/2.jpg",
+        "imagenes/finca1/3.jpg"
+    ]
+},
+
+{
+    id:2,
+    titulo:"Huerto en Corella",
+    comunidad:"Navarra",
+    provincia:"Navarra",
+    municipio:"Corella",
+
+    precio:90,
+
+    metros:600,
+
+    destacada:true,
+
+    imagenes:[
+        "imagenes/finca2/1.jpg",
+        "imagenes/finca2/2.jpg"
+    ]
+},
+
+{
+    id:3,
+    titulo:"Parcela en Alfaro",
+    comunidad:"La Rioja",
+    provincia:"La Rioja",
+    municipio:"Alfaro",
+
+    precio:150,
+
+    metros:700,
+
+    destacada:true,
+
+    imagenes:[
+        "imagenes/finca3/1.jpg"
+    ]
+},
+
+{
+    id:4,
+    titulo:"Parcela agrícola",
+    comunidad:"Aragón",
+    provincia:"Zaragoza",
+    municipio:"Tauste",
+
+    precio:100,
+
+    metros:800,
+
+    destacada:true,
+
+    imagenes:[
+        "imagenes/finca4/1.jpg"
+    ]
+},
+
+{
+    id:5,
+    titulo:"Finca de recreo",
+    comunidad:"Navarra",
+    provincia:"Navarra",
+    municipio:"Cintruénigo",
+
+    precio:110,
+
+    metros:450,
+
+    destacada:true,
+
+    imagenes:[
+        "imagenes/finca5/1.jpg"
+    ]
+},
+
+{
+    id:6,
+    titulo:"Terreno agrícola",
+    comunidad:"Navarra",
+    provincia:"Navarra",
+    municipio:"Cascante",
+
+    precio:130,
+
+    metros:1000,
+
+    destacada:true,
+
+    imagenes:[
+        "imagenes/finca6/1.jpg"
+    ]
+}
+
+];
